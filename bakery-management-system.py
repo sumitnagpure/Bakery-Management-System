5d233ae86dd7617c4611132c3f69fdbdffdab3dc
Sumit Nagpure <78203156+sumitnagpure@users.noreply.github.com>
2024-01-04 23:15:32 +0530

    Update bakery-management-system.py
    importing and initialization

--- bakery-management-system.py
+++ bakery-management-system.py
@@ -1,4 +1,6 @@
 import pandas as pd
 import numpy as np
+import datetime as datetime
 
-# bakery management system
+columns = ['Order_ID','Customer_Name','Order_Date','Food_Items','Quantities']
+orders_df=pd.DataFrame(columns=columns)
